1c267300921d20fc1cd2050e4fa5884717a4f394
Ahmad Zahid <73899902+ahmadZahid99@users.noreply.github.com>
2022-07-18 10:29:12 -0700

    same changes

--- longestWord.js
+++ longestWord.js
@@ -1,22 +1,26 @@
-let sample =
-  'Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers';
+function longestWord(sample) {
+  //replace or remove every char expect letter
+  sample = sample.replace(/[^a-zA-Z ]/g, '');
 
-//replace or remove every char expect letter
-sample = sample.replace(/[^a-zA-Z ]/g, '');
+  const words = sample.split(' ');
 
-const word = sample.split(' ');
+  let longest = '';
 
-let longest = '';
-
-for (let i = 0; i < word.length; i++) {
-  if (longest.length < word[i].length) {
-    longest = word[i];
-  } else if (longest.length == word[i].length) {
-    longest =
-      longest.match(/[aeiou]/g).lenght > word[i].match(/[aeiou]/g).length
-        ? longest
-        : word[i];
+  for (let i = 0; i < words.length; i++) {
+    if (longest.length < words[i].length) {
+      longest = words[i];
+    } else if (longest.length == words[i].length) {
+      longest =
+        longest.match(/[aeiou]/g).lenght > words[i].match(/[aeiou]/g).length
+          ? longest
+          : words[i];
+    }
   }
+  return longest;
 }
 
-console.log(longest);
+console.log(
+  longestWord(
+    'Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers'
+  )
+);
