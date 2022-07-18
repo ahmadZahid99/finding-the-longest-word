9600602ad1f57db440d22256f89f05cabb71e83b
Ahmad Zahid <73899902+ahmadZahid99@users.noreply.github.com>
2022-07-18 11:56:41 -0700

    samne changes

--- longestWord.js
+++ longestWord.js
@@ -1,9 +1,7 @@
 function longestWord(sample) {
   //replace or remove every char expect letter
   sample = sample.replace(/[^a-zA-Z ]/g, '');
-
   const words = sample.split(' ');
-
   let longest = '';
 
   for (let i = 0; i < words.length; i++) {
@@ -18,9 +16,15 @@ function longestWord(sample) {
   }
   return longest;
 }
-
 console.log(
   longestWord(
     'Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers'
+  )
+);
+console.log(longestWord("Don't be trapped in someone else's dream."));
+
+console.log(
+  longestWord(
+    'Like an echo in the forest The day will come back around As if nothing happened.'
   )
 );
